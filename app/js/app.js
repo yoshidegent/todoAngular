6e8d6f2deb408153e28ec9064e6ec50f05a3a301
YDEAX41 <yoshi_degent@hotmail.com>
2015-09-29 11:28:14 +0200

    added controller to the app.js 'todoController'

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,29 @@
-angular.module('todoApp', []);
+'use strict';
+
+var todoModule = angular.module('todoApp', []);
+
+todoModule.controller('todoController', function($scope){
+    $scope.title = "Yoshi's todo list";
+
+    var model =
+        [
+            {
+                description: 'Learn Javascript',
+                done: true
+            },
+            {
+                description: 'Learn angular',
+                done: false
+            },
+            {
+                description: 'Buy flowers',
+                done: false
+            },
+            {
+                description: 'Go shopping',
+                done: false
+            }
+        ];
+
+    $scope.data = model;
+});
